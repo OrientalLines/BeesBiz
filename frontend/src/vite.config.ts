be8ff2a7f008549939dcee2bbe10ca23fcf04b83
kxrxh <kxrxh.contact@gmail.com>
2024-11-07 13:46:22 +0300

    fix: paraglide vite build

--- frontend/src/vite.config.ts
+++ frontend/src/vite.config.ts
@@ -1,6 +1,7 @@
 import { paraglide } from '@inlang/paraglide-sveltekit/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import path from "node:path";
 
 export default defineConfig({
 	plugins: [
@@ -9,5 +10,10 @@ export default defineConfig({
 			project: './project.inlang',
 			outdir: './src/lib/paraglide'
 		})
-	]
+	],
+	resolve: {
+		alias: {
+			$paraglide: path.resolve(__dirname, "./src/paraglide"),
+		},
+	},
 });
