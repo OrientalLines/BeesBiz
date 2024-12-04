8615ac344ebf202ef10814b0ccde1d3740c790fe
Roomba, disciple of machine code <107208596+isofinly@users.noreply.github.com>
2024-12-04 23:00:10 +0300

    Update core.go

--- backend/src/internal/rest/core.go
+++ backend/src/internal/rest/core.go
@@ -136,6 +136,18 @@ func (s *Server) SetupRoutes() {
 	regionApiary.Get("/", handlers.GetAllRegionApiaries(s.db))
 
 	// User routes
+	user := api.Group("/user", roleMiddleware(types.Admin, types.Manager))
+	
+	user.Get("/:id", handlers.GetUser(s.db))
+	user.Post("/", handlers.CreateUser(s.db))
+	user.Put("/", handlers.UpdateUser(s.db))
+	user.Delete("/:id", handlers.DeleteUser(s.db))
+	user.Get("/", handlers.GetAllUsers(s.db))
+	user.Get("/:id/allowed-regions", handlers.GetUserAllowedRegions(s.db))
+	user.Put("/role", handlers.ModifyUserRole(s.db))
+	user.Put("/allowed-regions", handlers.ModifyUserAllowedRegions(s.db))
+	
+	workerGroup := api.Group("/worker-group", roleMiddleware(types.Admin, types.Manager))
 
 	workerGroup.Get("/:id", handlers.GetWorkerGroup(s.db))
 	workerGroup.Post("/", handlers.CreateWorkerGroup(s.db))
@@ -144,7 +156,6 @@ func (s *Server) SetupRoutes() {
 	workerGroup.Get("/", handlers.GetAllWorkerGroups(s.db))
 	workerGroup.Get("/manager/:manager_id", handlers.GetWorkerGroupsByManager(s.db))
 
-	user := api.Group("/worker-group", roleMiddleware(types.Admin, types.Manager))
 
 	// ProductionReport routes
 	productionReport := api.Group("/production-report", roleMiddleware(types.Manager, types.Worker, types.Admin))
